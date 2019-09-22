6a7432ec26792f4c47d49ac6a2904b2e5f6e1fea
github username <aude53@gmail.com>
2019-09-22 09:49:17 +0100

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-const testVar = {}
+const testVar = {};
 
 function testFunc() {
-  return "hi"
+  return "hi";
+}
+
+function superbowlWin(array) {
+  const findWin = array.find(function(element) {
+    if (element.result === "W") {
+      return element;
+    }
+  });
+
+  if (findWin) {
+    return findWin.year;
+  }
 }
